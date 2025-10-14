9cd28c32f6095e1341c0c1103a81c1a9616e6343
dkks112313 <ovcharenkoden05@gmail.com>
2025-10-14 12:38:46 +0300

    Add more specific code for buisness task

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
+
+type Metadata struct {
+	Path string `json:"page"`
+}
+
+type Event struct {
+	UserID   uint     `json:"user_id"`
+	Action   string   `json:"action"`
+	Metadata Metadata `json:"metadata"`
+}
+
+var savePlace []Event
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello")
+	})
+
+	http.HandleFunc("/event", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			if strings.HasPrefix(r.Header.Get("Content-Type"), "application/json") {
+				var event Event
+				decoder := json.NewDecoder(r.Body)
+				err := decoder.Decode(&event)
+				if err != nil {
+					fmt.Errorf("Error, when decode json")
+				}
+
+				println(event.UserID)
+				println(event.Action)
+				println(event.Metadata.Path)
+			}
+		}
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
